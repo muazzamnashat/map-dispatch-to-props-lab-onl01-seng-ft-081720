b0333b1d5b5ef2dcf235e1c64f63198a83008815
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-29 06:54:20 -0500

    commit tree 6f51cf07c6d68641abbba1aa8d816c5518d697f2

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant({name: this.state.name ,location: this.state.location})
   }
 
   render() {
@@ -49,6 +49,4 @@ export class RestaurantInput extends Component {
   }
 };
 
-
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null,{addRestaurant})(RestaurantInput); 
